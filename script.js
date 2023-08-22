d596a57588381deaa7ef11e78ef223e7140c3022
saikumarchittaluri <127985962+saikumarchittaluri@users.noreply.github.com>
2023-08-22 16:18:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+window.onload = function() {
+    // Get the element with the id "level"
+    var levelElement = document.getElementById("level");
+
+    // Calculate the level of the element
+    var level = 1;
+    var currentElement = levelElement;
+    while (currentElement.parentElement) {
+        currentElement = currentElement.parentElement;
+        if (currentElement.tagName === "UL") {
+            level++;
+        }
+    }
+
+    // Display the level using the alert function
+    alert("The level of the element is: " + level);
+};
+
